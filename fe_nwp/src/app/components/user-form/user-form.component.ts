dcff39e6a22f0636e4d7f08f7e8a629fd804bc61
duuunjaaa <62660866+duuunjaaa@users.noreply.github.com>
2025-10-25 21:21:59 +0200

    user form kinda but not really

--- fe_nwp/src/app/components/user-form/user-form.component.ts
+++ fe_nwp/src/app/components/user-form/user-form.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+export interface User {
+  id?: number;
+  name: string;
+  email: string;
+  role: string;
+}
 
 @Component({
   selector: 'app-user-form',
@@ -6,5 +14,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent {
+  @Input() user: User | null = null;
+  @Output() formSubmit = new EventEmitter<User>();
+
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      role: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.user) {
+      this.userForm.patchValue(this.user);
+    }
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      const userData: User = {
+        ...this.user,
+        ...this.userForm.value
+      };
+      this.formSubmit.emit(userData);
+    }
+  }
 
 }

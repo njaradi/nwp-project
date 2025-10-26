3f77e71f74ed689a3e127b0ef248cfc07e1c1f16
njaradi <injaradi6722rn@raf.rs>
2025-10-26 19:54:29 +0100

    new machine, filter

--- fe_nwp/src/app/components/machines-page/machines-page.component.ts
+++ fe_nwp/src/app/components/machines-page/machines-page.component.ts
@@ -8,6 +8,7 @@ import {VmCard} from "../../model";
   styleUrls: ['./machines-page.component.css']
 })
 export class MachinesPageComponent {
+  //TODO: make this list make sense, maybe put it in local storage
   vmList: VmCard[] = [
       { id: 1, name: 'VM Alpha', state: 'Running', date: '2025-10-20' },
       { id: 2, name: 'VM Beta', state: 'Stopped', date: '2025-10-22' },
@@ -17,37 +18,62 @@ export class MachinesPageComponent {
     ];
     nextId = 6;
 
-    nameInput = '';
-    stateInput = '';
-    dateFromInput = '';
-    dateToInput = '';
+     // Filters
+    filterName = '';
+    filterState = '';
+    filterDateFrom = '';
+    filterDateTo = '';
 
-    states = ['Running', 'Stopped', 'Crashed', 'Restarting'];
+    // Add new VM popup
+    showAddForm = false;
+    newVmName = '';
 
-    filterMachines(){
+    states = ['Running', 'Stopped', 'Crashed', 'Restarting', 'Turning off', 'Turning on'];
 
+      get filteredVmList(): VmCard[] {
+        return this.vmList.filter(vm => {
+              const matchesName =
+                !this.filterName || vm.name.toLowerCase().includes(this.filterName.toLowerCase());
+              const matchesState = !this.filterState || vm.state === this.filterState;
+              const matchesDateFrom = !this.filterDateFrom || vm.date >= this.filterDateFrom;
+              const matchesDateTo = !this.filterDateTo || vm.date <= this.filterDateTo;
+              return matchesName && matchesState && matchesDateFrom && matchesDateTo;
+            });
+      }
+
+    addVm() {
+      if (!this.newVmName.trim()) return;
+          this.vmList.push({
+            id: this.nextId++,
+            name: this.newVmName.trim(),
+            state: 'Stopped',
+            date: new Date().toISOString().slice(0, 10)
+          });
+          this.newVmName = '';
+          this.showAddForm = false;
     }
-    // addVm() {
-    //   if (!this.nameInput || !this.stateInput || !this.dateInput) return;
-    //   this.vmList.push({
-    //     id: this.nextId++,
-    //     name: this.nameInput,
-    //     state: this.stateInput,
-    //     date: this.dateInput
-    //   });
-    //   this.nameInput = '';
-    //   this.stateInput = '';
-    //   this.dateInput = '';
-    // }
+
+    cancelAdd() {
+      this.newVmName = '';
+      this.showAddForm = false;
+    }
 
     deleteVm(id: number) {
       this.vmList = this.vmList.filter(vm => vm.id !== id);
     }
 
     shutdown(vm: VmCard) {
-      vm.state = 'Stopped';
+      //maybe have timeout, inform user that it is in the process of turning off
+      vm.state = 'Turning off'
+      setTimeout(() => vm.state = 'Stopped', 2000)
     }
 
+    turnon(vm: VmCard){
+      vm.state = 'Turning on'
+      setTimeout(() => vm.state = 'Running', 2000)
+    }
+
+    //this should be turn on
     restart(vm: VmCard) {
       vm.state = 'Restarting';
       setTimeout(() => vm.state = 'Running', 2000);

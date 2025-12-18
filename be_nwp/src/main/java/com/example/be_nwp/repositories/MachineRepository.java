ec7ab047caadf84c9540074bf353793fa1ca5c51
njaradi <injaradi6722rn@raf.rs>
2025-12-18 13:15:32 +0100

    komentari

--- be_nwp/src/main/java/com/example/be_nwp/repositories/MachineRepository.java
+++ be_nwp/src/main/java/com/example/be_nwp/repositories/MachineRepository.java
@@ -6,4 +6,16 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MachineRepository extends CrudRepository<Machine, Long> {
+    //pretraga po nazivu, stanju, pocetni datum, krajnji datum
+    //napravi
+    // unisti  4p
+
+    // upali
+    //ugasi
+    //restart  4p
+
+
+
+    //zakazivanje? da li to ovde ili negde drugde zakazemo pa odatle
+    //samo pozovem neku od ovih operacija?  8p
 }

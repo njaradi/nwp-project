350920c450657d79ec9acd74a737ee792c2bc1fa
njaradi <injaradi6722rn@raf.rs>
2026-01-02 13:30:34 +0100

    changed springboot version

--- be_nwp/src/main/java/com/example/be_nwp/model/State.java
+++ be_nwp/src/main/java/com/example/be_nwp/model/State.java
@@ -1,5 +1,5 @@
 package com.example.be_nwp.model;
 
 public enum State {
-    RUNNING, CRASHED, RESTARTING, TURNING_OFF, TURNING_ON, STOPPED;
+    RUNNING, CRASHED, RESTARTING, TURNING_OFF, TURNING_ON, STOPPED
 }
